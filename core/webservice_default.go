6845d3a591ca1507a07ed5945bb1350791ebe75b
Andreas Genser <andreas.genser@bearingpoint.com>
2019-11-05 09:45:50 +0100

    adds Query function to webservice_default.go
    - Add the Query function from client version 2 to the webservice.

--- core/webservice_default.go
+++ core/webservice_default.go
@@ -32,6 +32,21 @@ func (c *Client) QueryWebservice(ws string, params map[string]string) (resp stri
 	return
 }
 
+// Query allows you to call a generic webservice(arg1: ws) with the providing params and a reference
+// to a result. It will take the built in resty function to deserialize the result
+// Return: error
+func (c *Client) Query(ws string, out interface{}, params map[string]string) (err error) {
+	if err = c.v2.Login(); err != nil {
+		return
+	}
+
+	if err = c.v2.Query(ws, out, params); err != nil {
+		log.Error("Error: ", err)
+	}
+
+	return
+}
+
 type ListOfCiIds []struct {
 	CiID int `json:"ciid,string"`
 }
