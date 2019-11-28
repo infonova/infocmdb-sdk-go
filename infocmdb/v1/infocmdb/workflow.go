36c1d9a5bf16113820a51edd688ddfbe746823ec
Michael Mitteregger <michael.mitteregger@bearingpoint.com>
2019-11-28 09:33:04 +0100

    deletes unused v1 workflow params

--- infocmdb/v1/infocmdb/workflow.go
+++ infocmdb/v1/infocmdb/workflow.go
@@ -1,10 +1,5 @@
 package infocmdb
 
-import (
-	"encoding/json"
-	"regexp"
-)
-
 const (
 	WORKFLOW_TRIGGER_TYPE_CI_CREATE = "ci_create"
 	WORKFLOW_TRIGGER_TYPE_CI_UPDATE = "ci_update"
@@ -26,29 +21,3 @@ const (
 	WORKFLOW_TRIGGER_TYPE_FILEIMPORT_AFTER            = "fileimport_after"
 	WORKFLOW_TRIGGER_TYPE_FILEIMPORT_BEFORE_AND_AFTER = "fileimport_before_and_after"
 )
-
-type WorkflowParams struct {
-	Apikey              string `json:"apikey"`
-	TriggerType         string `json:"triggerType"`
-	WorkflowItemId      int    `json:"workflow_item_id,string"`
-	WorkflowInstanceId  int    `json:"workflow_instance_id,string"`
-	CiId                int    `json:"ciid,string"`
-	CiAttributeId       int    `json:"ciAttributeId,string"`
-	CiRelationId        int    `json:"ciRelationId,string"`
-	CiProjectId         int    `json:"ciProjectId,string"`
-	FileImportHistoryId int    `json:"fileImportHistoryId,string"`
-}
-
-var reJsonParam = regexp.MustCompile(`^\s*'(.*?)\s*'$`)
-
-func ParseWorkflowParams(jsonParam string) (params WorkflowParams, err error) {
-	if reJsonParam.MatchString(jsonParam) {
-		jsonParam = reJsonParam.FindStringSubmatch(jsonParam)[1]
-	}
-	jsonErr := json.Unmarshal([]byte(jsonParam), &params)
-	if jsonErr != nil {
-		return params, jsonErr
-	}
-
-	return params, nil
-}
