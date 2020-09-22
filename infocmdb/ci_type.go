c267139290bb1456c2438426fa2285c29169c46d
Marko Oslaj <marko.oslaj@bearingpoint.com>
2020-09-22 09:06:33 +0200

    Adds function CreateCiType

--- infocmdb/ci_type.go
+++ infocmdb/ci_type.go
@@ -3,6 +3,7 @@ package infocmdb
 import (
 	"errors"
 	"strconv"
+	"strings"
 
 	v2 "github.com/infonova/infocmdb-sdk-go/infocmdb/v2/infocmdb"
 	utilError "github.com/infonova/infocmdb-sdk-go/util/error"
@@ -139,6 +140,195 @@ func (c *Client) SetTypeOfCi(ciId int, ciType string) (err error) {
 
 	if response.Success != true {
 		return errors.New("couldn't change ci type to: " + ciType + " for ciid: " + ciIdString + " ,error: " + response.Message)
+	}
+
+	return
+}
+
+type respCreateCiType struct {
+	Success bool         `json:"success"`
+	Message string       `json:"message"`
+	Data    []responseId `json:"data"`
+}
+
+type ciTypeParams struct {
+	Name                    string
+	Description             string
+	Note                    string
+	ParentCiTypeId          int
+	OrderNumber             int
+	CreateButtonDescription string
+	Icon                    string
+	Query                   string
+	DefaultProjectId        int
+	DefaultAttributeId      int
+	DefaultSortAttributeId  int
+	IsDefaultSortAsc        int
+	IsCiAttach              int
+	IsAttributeAttach       int
+	Tag                     string
+	IsTabEnabled            int
+	IsEventEnabled          int
+	IsActive                int
+	UserId                  int
+}
+
+func (c *Client) NewTypeParams(options ...func(*ciTypeParams)) *ciTypeParams {
+	typeParams := &ciTypeParams{
+		Name:                    "",
+		Description:             "",
+		Note:                    "",
+		ParentCiTypeId:          0,
+		OrderNumber:             0,
+		CreateButtonDescription: "",
+		Icon:                    "",
+		Query:                   "",
+		DefaultProjectId:        0,
+		DefaultAttributeId:      0,
+		DefaultSortAttributeId:  0,
+		IsDefaultSortAsc:        0,
+		IsCiAttach:              0,
+		IsAttributeAttach:       0,
+		Tag:                     "",
+		IsTabEnabled:            0,
+		IsEventEnabled:          0,
+		IsActive:                1,
+		UserId:                  0,
+	}
+
+	for _, option := range options {
+		option(typeParams)
+	}
+	return typeParams
+
+}
+
+func (c *Client) TypeWithName(name string) func(*ciTypeParams) {
+	return func(obj *ciTypeParams) {
+		obj.Name = name
+	}
+}
+
+func (c *Client) TypeWithDesc(desc string) func(*ciTypeParams) {
+	return func(obj *ciTypeParams) {
+		obj.Description = desc
+	}
+}
+
+func (c *Client) TypeWithNote(note string) func(*ciTypeParams) {
+	return func(obj *ciTypeParams) {
+		obj.Note = note
+	}
+}
+
+func (c *Client) TypeWithParentCiTypeId(parentId int) func(*ciTypeParams) {
+	return func(obj *ciTypeParams) {
+		obj.ParentCiTypeId = parentId
+	}
+}
+
+func (c *Client) TypeWithOrderNumber(orderNum int) func(*ciTypeParams) {
+	return func(obj *ciTypeParams) {
+		obj.OrderNumber = orderNum
+	}
+}
+
+func (c *Client) TypeWithDefProjectId(projId int) func(*ciTypeParams) {
+	return func(obj *ciTypeParams) {
+		obj.DefaultProjectId = projId
+	}
+}
+
+func (c *Client) TypeIsActive(active bool) func(*ciTypeParams) {
+	return func(obj *ciTypeParams) {
+		if active != true {
+			obj.IsActive = 0
+		}
+	}
+}
+
+func (c *Client) CreateCiType(typeParams *ciTypeParams) (typeId int, err error) {
+
+	if err = c.v2.Login(); err != nil {
+		return
+	}
+
+	typeIdExists, _ := c.GetCiTypeIdByCiTypeName(typeParams.Name)
+
+	if typeIdExists == 0 {
+
+		columns := []string{
+			"name",
+			"description",
+			"note",
+			"parent_ci_type_id",
+			"order_number",
+			"create_button_description",
+			"icon",
+			"query",
+			"default_project_id",
+			"default_attribute_id",
+			"default_sort_attribute_id",
+			"is_default_sort_asc",
+			"is_ci_attach",
+			"is_attribute_attach",
+			"tag",
+			"is_tab_enabled",
+			"is_event_enabled",
+			"is_active",
+			"user_id",
+		}
+
+		values := []string{
+			typeParams.Name,
+			typeParams.Description,
+			typeParams.Note,
+			strconv.Itoa(typeParams.ParentCiTypeId),
+			strconv.Itoa(typeParams.OrderNumber),
+			typeParams.CreateButtonDescription,
+			typeParams.Icon,
+			typeParams.Query,
+			strconv.Itoa(typeParams.DefaultProjectId),
+			strconv.Itoa(typeParams.DefaultAttributeId),
+			strconv.Itoa(typeParams.DefaultSortAttributeId),
+			strconv.Itoa(typeParams.IsDefaultSortAsc),
+			strconv.Itoa(typeParams.IsCiAttach),
+			strconv.Itoa(typeParams.IsAttributeAttach),
+			typeParams.Tag,
+			strconv.Itoa(typeParams.IsTabEnabled),
+			strconv.Itoa(typeParams.IsEventEnabled),
+			strconv.Itoa(typeParams.IsActive),
+			strconv.Itoa(typeParams.UserId),
+		}
+
+		params := map[string]string{
+			"argv1": "`" + strings.Join(columns, "`, `") + "`",
+			"argv2": "'" + strings.Join(values, "', '") + "'",
+		}
+
+		response := respCreateCiType{}
+		err = c.v2.Query("int_createCIType", &response, params)
+		if err != nil {
+			err = utilError.FunctionError(err.Error())
+			log.Error("Error: ", err)
+			return
+		}
+
+		if response.Success != true {
+			return 0, errors.New("error: " + response.Message)
+		}
+
+		switch len(response.Data) {
+		case 0:
+			err = utilError.FunctionError(typeParams.Name + " - " + v2.ErrNoResult.Error())
+		case 1:
+			typeId = response.Data[0].Id
+		default:
+			err = utilError.FunctionError(typeParams.Name + " - " + v2.ErrTooManyResults.Error())
+		}
+
+	} else {
+		return typeIdExists, nil
 	}
 
 	return
