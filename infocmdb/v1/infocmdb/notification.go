f196368b42ab0ba9ff51a5de5167180e6325ff4c
Michael Mitteregger <michael.mitteregger@bearingpoint.com>
2021-01-15 08:44:19 +0100

    Fix formatting of notification params
    The golang request header Add method fucks up the header names,
    so it needs to be avoided like a plague.

--- infocmdb/v1/infocmdb/notification.go
+++ infocmdb/v1/infocmdb/notification.go
@@ -59,20 +59,20 @@ func (i *Cmdb) SendNotification(notifyName string, params NotifyParams) (resp No
 		return resp, errors.New("failed to create a new request. Error: " + err.Error())
 	}
 
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header["Content-Type"] = []string{"application/x-www-form-urlencoded"}
 
 	if params.From != "" {
-		req.Header.Add("From", params.From)
+		req.Header["From"] = []string{params.From}
 	}
 
 	if params.FromName != "" {
-		req.Header.Add("FromName", params.FromName)
+		req.Header["FromName"] = []string{params.FromName}
 	}
 
 	allRecipients := strings.Join(params.Recipients, ";")
 
 	if allRecipients != "" {
-		req.Header.Add("Recipients", allRecipients)
+		req.Header["Recipients"] = []string{allRecipients}
 	}
 
 	recipientsCC := strings.Join(params.RecipientsCC, ";")
@@ -84,17 +84,17 @@ func (i *Cmdb) SendNotification(notifyName string, params NotifyParams) (resp No
 	recipientsBCC := strings.Join(params.RecipientsBCC, ";")
 
 	if recipientsBCC != "" {
-		req.Header.Add("RecipientsBCC", recipientsBCC)
+		req.Header["RecipientsBCC"] = []string{recipientsBCC}
 	}
 
 	allAttachments := strings.Join(params.AttachmentsPaths, ";")
 
 	if allAttachments != "" {
-		req.Header.Add("Attachments", allAttachments)
+		req.Header["Attachments"] = []string{allAttachments}
 	}
 
 	if params.Subject != "" {
-		req.Header.Add("Subject", params.Subject)
+		req.Header["Subject"] = []string{params.Subject}
 	}
 
 	response, err := httpClient.Do(req)
