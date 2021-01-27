51aefa959c98491ca3b7dc52a37a8d717629f5aa
Michael Mitteregger <michael.mitteregger@bearingpoint.com>
2021-01-27 15:48:47 +0100

    Fix lint errors in `infocmdb/v2/infocmdb/login_test.go`
    ´´´log
    [push/lint]   ❗  ::error file=infocmdb/v2/infocmdb/login_test.go,line=18,col=3::`password` is unused (structcheck)
    [push/lint]   ❗  ::error file=infocmdb/v2/infocmdb/login_test.go,line=16,col=3::`url` is unused (structcheck)
    [push/lint]   ❗  ::error file=infocmdb/v2/infocmdb/login_test.go,line=17,col=3::`username` is unused (structcheck)
    ´´´

--- infocmdb/v2/infocmdb/login_test.go
+++ infocmdb/v2/infocmdb/login_test.go
@@ -12,45 +12,34 @@ func TestInfoCMDB_LoginWithUserPass(t *testing.T) {
 	type fields struct {
 		Config Config
 	}
-	type args struct {
-		url      string
-		username string
-		password string
-	}
 	tests := []struct {
 		name    string
 		fields  fields
-		args    args
 		wantErr bool
 	}{
 		{
 			"valid login admin//admin",
 			fields{Config{Url: url, Username: "admin", Password: "admin"}},
-			args{},
 			false,
 		},
 		{
 			"invalid login admin//noadmin",
 			fields{Config{Url: url, Username: "admin", Password: "fail"}},
-			args{},
 			true,
 		},
 		{
 			"invalid no username",
 			fields{Config{Url: url, Username: "", Password: "fail"}},
-			args{},
 			true,
 		},
 		{
 			"invalid no password",
 			fields{Config: Config{Url: url, Username: "admin", Password: ""}},
-			args{},
 			true,
 		},
 		{
 			"invalid no data",
 			fields{Config: Config{Url: url, Username: "", Password: ""}},
-			args{},
 			true,
 		},
 	}
